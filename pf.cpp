200cb71d5c85b8554b3c498fc3259b1807f433b9
biah24 <bianca20062406@gmail.com>
2024-11-22 21:06:14 -0300

    Add files via upload

---
+++ pf.cpp
@@ -0,0 +1,33 @@
+// melhor filme para cada sala, ex: sala x rolando tal filme, sala y com tal filme
+//documentação, falar das funções e procedimentos, ex: temos a funç calcular reembolso, então tem que explicar tal função.
+//difine coluna e linha
+
+/*Reservar lugar.
+Exibir lugares disponíveis.
+Imprimir bilhete.
+Cancelar compra.
+Relatório de vendas.*/
+#include<stdio.>
+#define linhas 5
+#define coluna 5
+
+struct sala{
+		int salas [3];	
+		
+		salas[0] = 1;
+		salas[1] = 2;
+		salas[2] = 3;
+		
+};
+
+struct escolha_assento{
+	char assentos 
+	int bilhetes
+	
+	
+	
+};
+
+
+
+
